0b46ab65bfa7bae57fda2e47ec9514f0111868dc
20bear <20bear@stockholmscience.se>
2023-02-14 12:58:06 +0100

    Add the files that vscode forgot to add

---
+++ commands/command.js
@@ -0,0 +1,52 @@
+export class Command {
+	constructor() {
+		this.description = '';
+		this.usage = '';
+		this.args = {};
+		this.permission = null;
+		this.botPermissions = [];
+		this.guild = false;
+		this.cooldown = 0;
+		this.run = null;
+	}
+
+	setDescription(desc) {
+		this.description = desc;
+		return this;
+	}
+
+	setUsage(usage) {
+		this.usage = usage;
+		return this;
+	}
+
+	setArgs(args) {
+		this.args = args;
+		return this;
+	}
+
+	setPermission(perms) {
+		this.permission = perms;
+		return this;
+	}
+
+	setBotPermission(botPerms) {
+		this.botPermissions = botPerms;
+		return this;
+	}
+
+	setGuild(bool) {
+		this.guild = bool;
+		return this;
+	}
+
+	setCooldown(cd) {
+		this.cooldown = cd;
+		return this;
+	}
+
+	setRun(run) {
+		this.run = run;
+		return this;
+	}
+}
